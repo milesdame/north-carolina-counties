065e5c4d6a94a6db3a8dc9b46be5b3e44972557d
Miles Dame <milesedame@gmail.com>
2025-02-26 22:25:18 -0500

    Move functions for better organization

--- www/static/js/main.js
+++ www/static/js/main.js
@@ -5,10 +5,42 @@
 */
 export function removeCorrect(counties, guess) {
     // Get the index of the guessed county
-    let index = counties.indexOf(guess);
-    // Remove it
-    let modifiedCounties = counties.splice(index, 1);
-    // Return the modified array
-    return modifiedCounties;
+    return counties.filter(county => county !== guess);
+}
+
+export function alreadyGuessed(guess) {
+    console.log("MAKING IT HERE");
+    let paragraph = document.createElement("p");
+    paragraph.textContent = " Already Guessed " + guess + "!";
+    paragraph.setAttribute("class", "already-guessed");
+    document.getElementById("progress").appendChild(paragraph);
+
+}
+
+export function checkInput() {
+    let attemptRaw = document.getElementById("textInput").value;
+    console.log(attemptRaw);
+    let attempt = attemptRaw.toLowerCase();
+// each time the user types check the input value against the county list
+    if (counties.includes(attempt) && !correctCounties.includes(attempt)) {
+        console.log("YES");
+        correctCounties.push(attempt);
+        count++;
+        playerEntries.push(attempt)
+        document.getElementById("textInput").value = '';
+        console.log(count);
+        console.log(count.toString());
+        let element = document.getElementById(attempt);
+        let classes = element.classList;
+        classes.add("correct");
+        let total = document.createTextNode(count.toString());
+        let textStr = "Total: " + total.toString() + "/100 guessed";
+        let textNode = document.createTextNode(textStr);
+        document.getElementById("total").textContent = "Total: " + count + "/100 guessed";
+    } else if (correctCounties.includes(attempt)) {
+      console.log("ALREADY GUESSED"); 
+      alreadyGuessed(attempt);
+      document.getElementById("textInput").value = '';
+    }
 }
 
