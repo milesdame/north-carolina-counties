7d10fe9b8901f7c90093e7f5881f8cba6727fe08
Miles Dame <milesedame@gmail.com>
2025-02-26 22:26:03 -0500

    Add more tests

--- www/static/test/main.test.js
+++ www/static/test/main.test.js
@@ -4,5 +4,16 @@ test("removeCorrect should return correct array", () => {
     expect(removeCorrect(["ashe", "greene", "haywood", "henderson"], "greene")).toEqual(["ashe", "haywood", "henderson"]);
 });
 
+test("removeCorrect should return correct the same array if the guessed value is not contained", () => {
+    expect(removeCorrect(["ashe"], "greene")).toEqual(["ashe"]);
+});
 
+
+test("removeCorrect should return empty array if the guess is for the last item", () => {
+    expect(removeCorrect(["ashe"], "ashe")).toEqual([]);
+});
+
+test("removeCorrect should return empty array if there is a guess but the array is empty", () => {
+    expect(removeCorrect([], "ashe")).toEqual([]);
+});
   
