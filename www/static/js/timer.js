63fa4750e730ccc1bb0c845c96251adaa058c96c
Miles Dame <milesedame@gmail.com>
2025-02-28 21:37:32 -0500

    Adjust time to be a bit shorter

--- www/static/js/timer.js
+++ www/static/js/timer.js
@@ -1,6 +1,6 @@
 function timer () {
   // Set the date we're counting down to
-  var countDownDate = Date.now() + 600000;
+  var countDownDate = Date.now() + 480000;
   
   // Update the count down every 1 second
   var x = setInterval(function() {
